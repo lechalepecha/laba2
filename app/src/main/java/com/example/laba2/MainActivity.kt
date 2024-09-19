fdc341ae9e9d1e58de0ffe66a21a7a4303749f47
aleks <aleksejserakov45@gmail.com>
2024-09-19 18:17:05 +0700

    1

--- app/src/main/java/com/example/laba2/MainActivity.kt
+++ app/src/main/java/com/example/laba2/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.laba2
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun Solve(view: View){
+
+        val myNumberField = findViewById<EditText>(R.id.textNumber)
+        val inputValue = myNumberField.text.toString().toInt()
+
+        when(inputValue){
+            1 -> Toast.makeText(this, "Зима", Toast.LENGTH_SHORT).show()
+            2 -> Toast.makeText(this, "Весна", Toast.LENGTH_SHORT).show()
+            3-> Toast.makeText(this, "Лето", Toast.LENGTH_SHORT).show()
+            4 -> Toast.makeText(this, "Осень", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(this, "Ошибка, время года не существует", Toast.LENGTH_SHORT).show()
         }
     }
 }
